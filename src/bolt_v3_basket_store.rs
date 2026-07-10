c992bf442901ff30483612008b9f3f8014f6b42e
review <review@local>
2026-07-11 01:51:37 +0900

    test: revert compatibility proof mutations

--- src/bolt_v3_basket_store.rs
+++ src/bolt_v3_basket_store.rs
@@ -157,7 +157,6 @@ impl BoltV3BasketStore {
 #[derive(Debug, Serialize, Deserialize)]
 struct PersistedBasketState {
     schema_version: u32,
-    #[serde(rename = "state_v2")]
     state: BoltV3BasketExecutionState,
 }
 

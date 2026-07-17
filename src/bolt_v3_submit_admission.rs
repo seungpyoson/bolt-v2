b1a59d7116847d56a2c49e320b73f5dba252ab56
review <review@local>
2026-07-17 18:58:06 +0900

    Revert "test(1408): mutate primary poison recovery control"
    This reverts commit 5c49f71b99c2d3e211f3b104fe6b8c586d882b2f.

--- src/bolt_v3_submit_admission.rs
+++ src/bolt_v3_submit_admission.rs
@@ -1533,7 +1533,7 @@ impl BoltV3SubmitAdmissionState {
         let mut inner = self
             .inner
             .lock()
-            .unwrap_or_else(|poisoned| poisoned.into_inner());
+            .expect("submit admission state mutex should not be poisoned");
         let mut evaluation = self.evaluate(&mut inner, request, now_ns);
         let mut admitted_counter_update = None;
         if evaluation.outcome == BoltV3AdmissionOutcome::Admitted {
